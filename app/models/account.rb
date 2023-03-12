00a502278a57470a116616b7e411b3ba70cc4f68
Jesus Urias <waxxxave@pm.me>
2023-03-12 14:50:25 -0700

    Added clabe generation for account

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,4 +7,24 @@ class Account < ApplicationRecord
   alias cards card
   alias contacts contact
   alias operations operation
+
+  validates :name, presence: true
+  validates :phone, presence: true, length: { is: 10 }
+  validates :password, presence: true
+  validates :clabe, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  before_create :clabe_assignation
+
+  private
+
+  def clabe_assignation
+    self.clabe = clabe_generator
+  end
+
+  def clabe_generator(length = 4)
+    a_ascii_char_code = 'A'.ord
+    z_ascii_char_code = 'Z'.ord
+    (0...length).map { rand(a_ascii_char_code..z_ascii_char_code).chr }.join
+  end
 end
