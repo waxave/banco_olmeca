68ad4d823be8484fb3a467a38eaea3d8ca5a22b4
Jesus Urias <waxxxave@pm.me>
2023-03-12 19:30:32 -0700

    Modified Operation model tests to use fixtures

--- test/models/operation_test.rb
+++ test/models/operation_test.rb
@@ -4,48 +4,16 @@ class OperationTest < ActiveSupport::TestCase
   include BCrypt
 
   test 'create a new deposit operation without errors' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @card = Card.create(
-      pin: 8901,
-      balance: 9000,
-      account: @account
-    )
-
-    @operation = Operation.create(
-      account: @account,
-      operationable: @card,
-      kind: :deposit,
-      amount: 100,
-      concept: 'new tranfer'
-    )
+    @operation = operations(:success)
 
     assert_empty(@operation.errors)
   end
 
   test 'create a new withdrawal operation without errors' do
-    @starting_balance = 5000
+    @account = accounts(:one)
+    @card = cards(:debit)
     @withdrawal_quantity = 3000
-
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @card = Card.create(
-      pin: 8901,
-      balance: @starting_balance,
-      account: @account
-    )
+    @starting_balance = @card.balance
 
     @operation = Operation.create(
       account: @account,
@@ -60,25 +28,14 @@ class OperationTest < ActiveSupport::TestCase
   end
 
   test 'return an error when the account does not have enough funds to withdraw' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @card = Card.create(
-      pin: 8901,
-      balance: 1000,
-      account: @account
-    )
+    @account = accounts(:one)
+    @card = cards(:debit)
 
     @operation = Operation.create(
       account: @account,
       operationable: @card,
       kind: :withdrawal,
-      amount: 1500,
+      amount: 89_798,
       concept: 'new withdrawal'
     )
 
@@ -87,28 +44,14 @@ class OperationTest < ActiveSupport::TestCase
   end
 
   test 'create a new transfer operation without errors' do
-    @starting_balance_account = 15000
-    @starting_balance_account_two = 5000
+    @account_one = accounts(:one)
+    @account_two = accounts(:two)
+    @starting_balance_account_one = @account_one.balance
+    @starting_balance_account_two = @account_two.balance
     @withdrawal_quantity = 3000
 
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: @starting_balance_account,
-      password: Password.create('meh')
-    )
-
-    @account_two = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'account_two@example.com',
-      balance: @starting_balance_account_two,
-      password: Password.create('meh')
-    )
-
     @operation = Operation.create(
-      account: @account,
+      account: @account_one,
       operationable: @account_two,
       kind: :transfer,
       amount: @withdrawal_quantity,
@@ -116,36 +59,22 @@ class OperationTest < ActiveSupport::TestCase
     )
 
     assert_empty(@operation.errors)
-    assert_equal(@account.balance, @starting_balance_account - @withdrawal_quantity)
+    assert_equal(@account_one.balance, @starting_balance_account_one - @withdrawal_quantity)
     assert_equal(@account_two.balance, @starting_balance_account_two + @withdrawal_quantity)
   end
 
   test 'return an error when the account does not have enough funds to transfer' do
-    @starting_balance_account = 1000
-    @starting_balance_account_two = 5000
-    @withdrawal_quantity = 3000
-
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: @starting_balance_account,
-      password: Password.create('meh')
-    )
-
-    @account_two = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'account_two@example.com',
-      balance: @starting_balance_account_two,
-      password: Password.create('meh')
-    )
+    @account_one = accounts(:one)
+    @account_two = accounts(:two)
+    @starting_balance_account_one = @account_one.balance
+    @starting_balance_account_two = @account_two.balance
+    @transfer_quantity = 450_000
 
     @operation = Operation.create(
-      account: @account,
+      account: @account_one,
       operationable: @account_two,
       kind: :transfer,
-      amount: @withdrawal_quantity,
+      amount: @transfer_quantity,
       concept: 'new withdrawal'
     )
 
