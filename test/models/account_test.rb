c4205b2d735f83545eabce3d08bb9bb87d23843a
Jesus Urias <waxxxave@pm.me>
2023-03-12 18:30:12 -0700

    Moved Account tests to use fixtures

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -4,46 +4,21 @@ class AccountTest < ActiveSupport::TestCase
   include BCrypt
 
   test 'create a new account without errors' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
+    @account = accounts(:one)
 
     assert_empty(@account.errors)
   end
 
   test 'has errors when email has been taken' do
-    Account.create!(
-      name: 'account one',
-      phone: '0123456789',
-      email: 'email@error.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @account_two = Account.create(
-      name: 'account two',
-      phone: '0123456789',
-      email: 'email@error.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
+    @account_one = accounts(:one)
+    @account_two = accounts(:email_repeated)
 
     assert_equal(@account_two.valid?, false)
     assert_includes(@account_two.errors[:email], 'has already been taken')
   end
 
   test 'has errors when phone is not correct' do
-    @account = Account.create(
-      name: 'account with phone errors',
-      phone: '01',
-      email: 'phone@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
+    @account = accounts(:phone_incorrect)
 
     assert_equal(@account.valid?, false)
     assert_includes(@account.errors[:phone], 'is the wrong length (should be 10 characters)')
