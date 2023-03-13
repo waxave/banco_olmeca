e9cc212c05ceccfd7d0349bac570d7619ef6f087
Jesus Urias <waxxxave@pm.me>
2023-03-13 16:43:10 -0700

    Moved sign in to his own controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'accounts#index'
 
   resources :accounts
+  resources :sign_in, only: %i[new create]
 end
