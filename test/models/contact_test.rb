8f06a5a6e6bcdb708be9630235338998ac938da9
Jesus Urias <waxxxave@pm.me>
2023-03-12 19:00:40 -0700

    Modified Contact tests to use fixtures

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -4,76 +4,20 @@ class ContactTest < ActiveSupport::TestCase
   include BCrypt
 
   test 'create a new contact without errors' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @card = Card.create(
-      pin: 8901,
-      balance: 9000,
-      account: @account
-    )
-
-    @contact = Contact.create(
-      name: "new contact for: #{@card.number}",
-      account: @account,
-      contactable: @card
-    )
+    @contact = contacts(:account_one)
 
     assert_empty(@contact.errors)
   end
 
   test 'create a new contact without errors for Card' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @card = Card.create(
-      pin: 8901,
-      balance: 9000,
-      account: @account
-    )
-
-    @contact = Contact.create(
-      name: "new contact for: #{@card.number}",
-      account: @account,
-      contactable: @card
-    )
+    @contact = contacts(:card_debit)
 
     assert_empty(@contact.errors)
     assert_equal(@contact.contactable_type, 'Card')
   end
 
   test 'create a new contact without errors for Account' do
-    @account = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'without@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @account_two = Account.create(
-      name: 'account without errors',
-      phone: '0123456789',
-      email: 'account_two_without_errors@errors.com',
-      balance: 1000,
-      password: Password.create('meh')
-    )
-
-    @contact = Contact.create(
-      name: "new contact for: #{@account_two.clabe}",
-      account: @account,
-      contactable: @account_two
-    )
+    @contact = contacts(:account_one)
 
     assert_empty(@contact.errors)
     assert_equal(@contact.contactable_type, 'Account')
