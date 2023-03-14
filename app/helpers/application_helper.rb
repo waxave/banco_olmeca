b3c204291e32d535e8e1f2f4cc91fbbe08aa7fa5
Jesus Urias <waxxxave@pm.me>
2023-03-13 19:49:34 -0700

    Added balance and card in dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def page
     'Dashboard'
   end
+
+  def display_card(card_number)
+    return '0000-0000-0000-0000' unless card_number.present?
+
+    card_number.scan(/.{1,4}/).join('-')
+  end
 end
